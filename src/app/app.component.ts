3690b7fb38a812d58c16ec76abbb7a01f5fd9ba5
ankur01oct <ankur01oct@gmail.com>
2018-05-27 22:45:10 +0300

    FirstCommit from Local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { PharmacyService } from './pharmacy/pharmacy.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [PharmacyService]
 })
 export class AppComponent {
-  title = 'app';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { AddStockComponent } from './pharmacy/add-stock.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
+import { DisplayTableComponent } from './pharmacy/display-table.component';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayTableComponent,
+    AddStockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
